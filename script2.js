c09b5858779bfd5baaa04099150ee44ccb8f22b5
Ahmadusoap <114605472+Ahmadusoap@users.noreply.github.com>
2022-10-21 09:46:53 +0800

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,32 +1,37 @@
-function login()
-	{
-		var uname = document.getElementById("email").value;
-		var pwd = document.getElementById("pwd1").value;
-		var filter = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
-		if(uname =='')
-		{
-			alert("Please enter a username.");
-		}
-		else if(pwd=='')
-		{
-        	alert("Please enter a password");
-		}
-		else if(!filter.test(uname))
-		{
-			alert("Please enter a valid username. (Need to include atleast ONE uppercase, lowercase, number and symbol)");
-		}
-		else if(pwd.length < 12)
-		{
-			alert("Password min length is 12.");
-		}
-		else
-		{
-	alert('Successful Login!');
-		}
+function login() {
+	var uname = document.getElementById("email").value;
+	var pwd = document.getElementById("pwd1").value;
+	var filterH = /(?=.*[A-Z])/;
+	var filterh = /(?=.*[a-z])/;
+	var filternum = /(?=.*[0-9])/;
+	var filtersym = /(?=.*\d)/;
+	if (uname == '') {
+		alert("Please enter a username.");
 	}
-    
-	function clearFunc()
-	{
-		document.getElementById("email").value="";
-		document.getElementById("pwd1").value="";
+	else if (pwd == '') {
+		alert("Please enter a password");
 	}
+	else if (!filterH.test(pwd)) {
+		alert("The password need to include atleast ONE uppercase.");
+	}
+	else if (!filterh.test(pwd)) {
+		alert("The password need to include atleast ONE lowercase.");
+	}
+	else if (pwd.length < 12) {
+		alert("Password minimum length is 12.");
+	}
+	else if (!filternum.test(pwd)) {
+		alert("The password need to include atleast ONE number.");
+	}
+	else if (!filtersym.test(pwd)) {
+		alert("The password need to include atleast ONE symbol.");
+	}
+	else {
+		alert('Successful Login!');
+	}
+}
+
+function clearFunc() {
+	document.getElementById("email").value = "";
+	document.getElementById("pwd1").value = "";
+}
